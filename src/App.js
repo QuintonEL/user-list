0eaf38c414bc0b78a0a7df452432834ba28e0f4f
Quinton LaBorde <quintonlaborde@gmail.com>
2022-05-31 13:09:53 -0600

    added a users list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]}/>
     </div>
   );
 }
